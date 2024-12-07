3a76fdd18380be9b60d46731cf0203adbcd76296
Atakan Botasun <atakanbotasun@outlook.com>
2024-12-07 17:53:21 +0300

    first working atari dqn implementation

---
+++ src/sb3_minigrid_dqn.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+FNAME = "minigrid_empty_16x16_plain_dqn_2"
+import numpy as np
+
+import torch
+import torch.nn as nn
+
+from stable_baselines3 import DQN
+from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
+from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.evaluation import evaluate_policy
+
+import gymnasium as gym
+import minigrid
+from minigrid.wrappers import ImgObsWrapper
+
+
+# class MinigridFeaturesExtractor(BaseFeaturesExtractor):
+#     def __init__(self, observation_space: gym.Space, features_dim: int = 512, normalized_image: bool = False) -> None:
+#         super().__init__(observation_space, features_dim)
+#         n_input_channels = observation_space.shape[0]
+#         self.cnn = nn.Sequential(
+#             nn.Conv2d(n_input_channels, 16, (2, 2)),
+#             nn.ReLU(),
+#             nn.Conv2d(16, 32, (2, 2)),
+#             nn.ReLU(),
+#             nn.Conv2d(32, 64, (2, 2)),
+#             nn.ReLU(),
+#             nn.Flatten(),
+#         )
+
+#         # Compute shape by doing one forward pass
+#         with torch.no_grad():
+#             n_flatten = self.cnn(torch.as_tensor(observation_space.sample()[None]).float()).shape[1]
+
+#         self.linear = nn.Sequential(nn.Linear(n_flatten, features_dim), nn.ReLU())
+
+#     def forward(self, observations: torch.Tensor) -> torch.Tensor:
+#         return self.linear(self.cnn(observations))
+
+# policy_kwargs = dict(
+#     features_extractor_class=MinigridFeaturesExtractor,
+#     features_extractor_kwargs=dict(features_dim=128),
+# )
+
+env = gym.make("MiniGrid-Empty-16x16-v0", render_mode="rgb_array")
+# env = ImgObsWrapper(env)
+
+
+model = DQN("MlpPolicy", env, verbose=1)
+model.learn(2e5)
+
+mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=1000)
+vec_env = model.get_env()
+obs = vec_env.reset()
+for i in range(1000):
+    action, _states = model.predict(obs, deterministic=True)
+    obs, rewards, dones, info = vec_env.step(action)
+    vec_env.render("rgb_array")
+
+print(mean_reward, std_reward)
+
+model.save(f"./models/{FNAME}.pt")
